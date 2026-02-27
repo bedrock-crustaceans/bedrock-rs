636be81e5c8dea1815c384c5f8633a21f2e2bbd9
xRookieFight <me@xrookiefight.com>
2026-02-27 11:19:32 +0300

    Camera aim assist preset changes

--- crates/proto/src/version/v800/packets/camera_aim_assist_presets.rs
+++ crates/proto/src/version/v800/packets/camera_aim_assist_presets.rs
@@ -1,4 +1,5 @@
 use bedrockrs_macros::{gamepacket, ProtoCodec};
+use crate::v800::types::CameraAimAssistCategory;
 use super::super::enums::CameraAimAssistOperation;
 use super::super::types::{CameraAimAssistCategories, CameraAimAssistPresetDefinition};
 
@@ -8,6 +9,9 @@ pub struct CameraAimAssistPresetsPacket {
     #[vec_repr(i32)]
     #[vec_endianness(var)]
     pub categories: Vec<CameraAimAssistCategories>,
+    #[vec_repr(i32)]
+    #[vec_endianness(var)]
+    pub category_definitions: Vec<CameraAimAssistCategory>,
     #[vec_repr(i32)]
     #[vec_endianness(var)]
     pub presets: Vec<CameraAimAssistPresetDefinition>,
