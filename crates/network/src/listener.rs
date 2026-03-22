4da7e1f75a9ab19135282d8bb673f29f3ac89da2
OmniacDev <64994204+OmniacDev@users.noreply.github.com>
2026-03-22 17:23:36 +1300

    fix: remove ListenerBuilder

--- crates/network/src/listener.rs
+++ crates/network/src/listener.rs
@@ -10,22 +10,6 @@ use crate::transport::TransportLayerListener;
 use bedrockrs_proto::Unknown;
 use bedrockrs_proto::info::MINECRAFT_EDITION_MOTD;
 
-pub struct ListenerBuilder {
-    address: SocketAddress,
-}
-
-impl ListenerBuilder {
-    pub fn new() -> Self {}
-
-    pub fn build(self) -> Result<Listener, ListenerError> {}
-}
-
-impl Default for ListenerBuilder {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
 pub struct Listener {
     listener: TransportLayerListener,
     name: String,
