d3ae60fb5f88f47d820c0283bf8f08b18c5640e3
OmniacDev <64994204+OmniacDev@users.noreply.github.com>
2026-03-22 17:28:08 +1300

    fix: Listener dead_code warning

--- crates/network/src/listener.rs
+++ crates/network/src/listener.rs
@@ -10,6 +10,7 @@ use crate::transport::TransportLayerListener;
 use bedrockrs_proto::Unknown;
 use bedrockrs_proto::info::MINECRAFT_EDITION_MOTD;
 
+#[allow(dead_code)]
 pub struct Listener {
     listener: TransportLayerListener,
     name: String,
