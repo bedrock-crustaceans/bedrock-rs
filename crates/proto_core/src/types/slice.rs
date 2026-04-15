dfdfae555b9c78ac826ccba046cb3eb1d406e3ba
Ruben <44266876+RadiatedMonkey@users.noreply.github.com>
2026-04-15 17:25:14 +0200

    port ProtoCodecLE, ProtoCodecBE and ProtoCodecVAR

--- crates/proto_core/src/types/slice.rs
+++ crates/proto_core/src/types/slice.rs
@@ -1,7 +1,6 @@
 use crate::ProtoCodec;
 use crate::endian::{ProtoCodecBE, ProtoCodecLE, ProtoCodecVAR};
 use crate::error::ProtoCodecError;
-use seq_macro::seq;
 use std::io::{Read, Write};
 use std::mem::MaybeUninit;
 
@@ -26,9 +25,9 @@ impl<T, const N: usize> ArrayInitGuard<T, N> {
     ///
     /// If a call to `op` fails or panics, this function will return the error and all previously initialized
     /// items will be dropped.
-    pub fn initialize<F: Fn() -> Result<T, ProtoCodecError>>(
+    pub fn initialize<F: FnMut() -> Result<T, ProtoCodecError>>(
         &mut self,
-        op: F,
+        mut op: F,
     ) -> Result<[T; N], ProtoCodecError> {
         self.array.iter_mut().try_for_each(|x| {
             let init = op()?;
@@ -77,87 +76,56 @@ impl<T: ProtoCodec, const N: usize> ProtoCodec for [T; N] {
     }
 }
 
-macro_rules! impl_proto_slice {
-    ($name:ident, 0) => {
-        impl<T> $name for [T; 0] {
-            fn serialize<W: Write>(&self, _stream: &mut W) -> Result<(), ProtoCodecError> {
-                Ok(())
-            }
+impl<T: ProtoCodecLE, const N: usize> ProtoCodecLE for [T; N] {
+    fn serialize<W: Write>(&self, stream: &mut W) -> Result<(), ProtoCodecError> {
+        self.iter().try_for_each(|x| x.serialize(stream))
+    }
 
-            fn deserialize<R: Read>(_stream: &mut R) -> Result<Self, ProtoCodecError> {
-                Ok([])
-            }
+    fn deserialize<R: Read>(stream: &mut R) -> Result<Self, ProtoCodecError> {
+        // FIXME: We can use the safe equivalent below when the `array_try_from_fn` feature is stabilised.
+        // std::array::try_from_fn(|| T::deserialize(stream))
 
-            fn size_hint(&self) -> usize {
-                0
-            }
-        }
-    };
-    ($name:ident, $size:literal) => {
-        impl<T: $name> $name for [T; $size] {
-            fn serialize<W: Write>(&self, stream: &mut W) -> Result<(), ProtoCodecError> {
-                seq!(N in 0..$size {
-                    self[N].serialize(stream)?;
-                });
+        let mut guard = const { ArrayInitGuard::new() };
+        guard.initialize(|| T::deserialize(stream))
+    }
 
-                Ok(())
-            }
-
-            fn deserialize<R: Read>(stream: &mut R) -> Result<Self, ProtoCodecError> {
-                seq!(N in 0..$size {
-                    let buf = [
-                        #( T::deserialize(stream)?, )*
-                    ];
-                });
-
-                Ok(buf)
-            }
-
-            fn size_hint(&self) -> usize {
-                self.iter().fold(0, |acc, x| acc + x.size_hint())
-            }
-        }
-    };
+    fn size_hint(&self) -> usize {
+        self.iter().fold(0, |acc, x| acc + x.size_hint())
+    }
 }
 
-impl_proto_slice!(ProtoCodecLE, 0);
-impl_proto_slice!(ProtoCodecLE, 1);
-impl_proto_slice!(ProtoCodecLE, 2);
-impl_proto_slice!(ProtoCodecLE, 3);
-impl_proto_slice!(ProtoCodecLE, 4);
-impl_proto_slice!(ProtoCodecLE, 5);
-impl_proto_slice!(ProtoCodecLE, 6);
-impl_proto_slice!(ProtoCodecLE, 7);
-impl_proto_slice!(ProtoCodecLE, 8);
-impl_proto_slice!(ProtoCodecLE, 9);
-impl_proto_slice!(ProtoCodecLE, 10);
-impl_proto_slice!(ProtoCodecLE, 11);
-impl_proto_slice!(ProtoCodecLE, 12);
+impl<T: ProtoCodecBE, const N: usize> ProtoCodecBE for [T; N] {
+    fn serialize<W: Write>(&self, stream: &mut W) -> Result<(), ProtoCodecError> {
+        self.iter().try_for_each(|x| x.serialize(stream))
+    }
 
-impl_proto_slice!(ProtoCodecBE, 0);
-impl_proto_slice!(ProtoCodecBE, 1);
-impl_proto_slice!(ProtoCodecBE, 2);
-impl_proto_slice!(ProtoCodecBE, 3);
-impl_proto_slice!(ProtoCodecBE, 4);
-impl_proto_slice!(ProtoCodecBE, 5);
-impl_proto_slice!(ProtoCodecBE, 6);
-impl_proto_slice!(ProtoCodecBE, 7);
-impl_proto_slice!(ProtoCodecBE, 8);
-impl_proto_slice!(ProtoCodecBE, 9);
-impl_proto_slice!(ProtoCodecBE, 10);
-impl_proto_slice!(ProtoCodecBE, 11);
-impl_proto_slice!(ProtoCodecBE, 12);
+    fn deserialize<R: Read>(stream: &mut R) -> Result<Self, ProtoCodecError> {
+        // FIXME: We can use the safe equivalent below when the `array_try_from_fn` feature is stabilised.
+        // std::array::try_from_fn(|| T::deserialize(stream))
 
-impl_proto_slice!(ProtoCodecVAR, 0);
-impl_proto_slice!(ProtoCodecVAR, 1);
-impl_proto_slice!(ProtoCodecVAR, 2);
-impl_proto_slice!(ProtoCodecVAR, 3);
-impl_proto_slice!(ProtoCodecVAR, 4);
-impl_proto_slice!(ProtoCodecVAR, 5);
-impl_proto_slice!(ProtoCodecVAR, 6);
-impl_proto_slice!(ProtoCodecVAR, 7);
-impl_proto_slice!(ProtoCodecVAR, 8);
-impl_proto_slice!(ProtoCodecVAR, 9);
-impl_proto_slice!(ProtoCodecVAR, 10);
-impl_proto_slice!(ProtoCodecVAR, 11);
-impl_proto_slice!(ProtoCodecVAR, 12);
+        let mut guard = const { ArrayInitGuard::new() };
+        guard.initialize(|| T::deserialize(stream))
+    }
+
+    fn size_hint(&self) -> usize {
+        self.iter().fold(0, |acc, x| acc + x.size_hint())
+    }
+}
+
+impl<T: ProtoCodecVAR, const N: usize> ProtoCodecVAR for [T; N] {
+    fn serialize<W: Write>(&self, stream: &mut W) -> Result<(), ProtoCodecError> {
+        self.iter().try_for_each(|x| x.serialize(stream))
+    }
+
+    fn deserialize<R: Read>(stream: &mut R) -> Result<Self, ProtoCodecError> {
+        // FIXME: We can use the safe equivalent below when the `array_try_from_fn` feature is stabilised.
+        // std::array::try_from_fn(|| T::deserialize(stream))
+
+        let mut guard = const { ArrayInitGuard::new() };
+        guard.initialize(|| T::deserialize(stream))
+    }
+
+    fn size_hint(&self) -> usize {
+        self.iter().fold(0, |acc, x| acc + x.size_hint())
+    }
+}
