b53b4b4d960e608eb17c76a796a0f81680b750cc
OmniacDev <64994204+OmniacDev@users.noreply.github.com>
2026-06-14 23:42:37 +1200

    style: fmt

--- crates/protocol_defs/src/schemas/version.rs
+++ crates/protocol_defs/src/schemas/version.rs
@@ -25,31 +25,31 @@ pub enum Change {
     Added {
         name: Box<str>,
         #[serde(default)]
-        versioned: bool
+        versioned: bool,
     },
     Modified {
         name: Box<str>,
         #[serde(default)]
-        versioned: bool
+        versioned: bool,
     },
     Removed {
         name: Box<str>,
-    }
+    },
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::schemas::version::Version;
     use crate::ProtocolDefs;
+    use crate::schemas::version::Version;
     use ron::from_str;
 
     #[test]
     fn unknown_version() {
         let file = ProtocolDefs::get("unknown/version.ron").unwrap();
         let str = str::from_utf8(&file.data).unwrap();
-        
+
         let version = from_str::<Version>(str).unwrap();
-        
+
         println!("{:?}", version);
     }
 }
