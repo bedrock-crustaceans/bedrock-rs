173381a9a4de01894aca3e0814bcc22b2ddb0a12
OmniacDev <64994204+OmniacDev@users.noreply.github.com>
2026-07-10 19:01:32 +1200

    fix: PlayerAuthInput move_vector should be (f32, f32)

--- crates/protocol/src/version/v766/packets/player_auth_input.rs
+++ crates/protocol/src/version/v766/packets/player_auth_input.rs
@@ -12,7 +12,7 @@ use std::io::{Read, Write};
 pub struct PlayerAuthInputPacket<V: ProtoVersion> {
     pub player_rotation: (f32, f32),
     pub player_position: (f32, f32, f32),
-    pub move_vector: (f32, f32, f32),
+    pub move_vector: (f32, f32),
     pub player_head_rotation: f32,
     pub input_data: u128,
     pub input_mode: V::InputMode,
@@ -134,7 +134,7 @@ impl<V: ProtoVersion> ProtoCodec for PlayerAuthInputPacket<V> {
     fn serialize<W: Write>(&self, stream: &mut W) -> Result<(), ProtoCodecError> {
         <(f32, f32) as ProtoCodecLE>::serialize(&self.player_rotation, stream)?;
         <(f32, f32, f32) as ProtoCodecLE>::serialize(&self.player_position, stream)?;
-        <(f32, f32, f32) as ProtoCodecLE>::serialize(&self.move_vector, stream)?;
+        <(f32, f32) as ProtoCodecLE>::serialize(&self.move_vector, stream)?;
         <f32 as ProtoCodecLE>::serialize(&self.player_head_rotation, stream)?;
         <u128 as ProtoCodecVAR>::serialize(&self.input_data, stream)?;
         <V::InputMode as ProtoCodec>::serialize(&self.input_mode, stream)?;
@@ -180,7 +180,7 @@ impl<V: ProtoVersion> ProtoCodec for PlayerAuthInputPacket<V> {
     fn deserialize<R: Read>(stream: &mut R) -> Result<Self, ProtoCodecError> {
         let player_rotation = <(f32, f32) as ProtoCodecLE>::deserialize(stream)?;
         let player_position = <(f32, f32, f32) as ProtoCodecLE>::deserialize(stream)?;
-        let move_vector = <(f32, f32, f32) as ProtoCodecLE>::deserialize(stream)?;
+        let move_vector = <(f32, f32) as ProtoCodecLE>::deserialize(stream)?;
         let player_head_rotation = <f32 as ProtoCodecLE>::deserialize(stream)?;
         let input_data = <u128 as ProtoCodecVAR>::deserialize(stream)?;
         let input_mode = <V::InputMode as ProtoCodec>::deserialize(stream)?;
